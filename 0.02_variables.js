36257dc138da9acf530d3ee40e27762ef5c3f307
Clare Danner <cadanner15@my.trine.edu>
2017-06-21 15:36:21 -0400

    added js 0.02

---
+++ 0.02_variables.js
@@ -0,0 +1,35 @@
+var x = 2
+console.log(x)
+
+var y = 10
+var z = x + y
+console.log(z);
+
+//Naming
+//Name variables anything
+var year = 2017;
+var siblings = 2000;
+
+//Camel Casing
+var yearTrineWasFounded = 1884;
+var theAgeOfMyMom = 50;
+var newNumber = (z* yearTrineWasFounded)
+console.log(newNumber);
+
+//Shorthand Operators
+year -= 10;
+//same as 
+year = year -10;
+
+console.log(year);
+
+siblings % 200;
+//same as siblings = siblings % 200
+ console.log(siblings);
+
+ //Using the var for a value interchangeable
+ var numberOfYearsTrineHasExisted = year + 133;
+ console.log(numberOfYearsTrineHasExisted);
+ var three = 3;
+ console.log (three * three);
+ console.log(three)
