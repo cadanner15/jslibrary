1ef2bdcc6fbac22c1c31c6a399932729c26bd8dc
Clare Danner <cadanner15@my.trine.edu>
2017-06-21 15:01:58 -0400

    added js 0.00

---
+++ 0.00_setup.js
@@ -0,0 +1,20 @@
+console.log("Hi, friends!")
+
+//int/long
+console.log(3)
+//decimal/double/float
+console.log(3.3);
+console.log(3+3);
+console.log(3.3*3.3);
+
+//Operators
+console.log(4+4);
+console.log(5-5);
+console.log(6*6);
+console.log(7/7);
+
+//Mod
+console.log(12%6);
+console.log(12%6);
+
+console.log(8+8.1)
